2def85202faac1703c6ce5578ecaa43da7a4ef39
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-30 17:01:03 +0300

    Updates

---
+++ precache-manifest.af8da173c5c7bee38e71cbf48dc0590c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379a00d48aebd98303209085005d7e8d",
+    "url": "/Artem20201610/react_people-table-advanced/index.html"
+  },
+  {
+    "revision": "af95ec4e54be896ff0c4",
+    "url": "/Artem20201610/react_people-table-advanced/static/css/2.1890a892.chunk.css"
+  },
+  {
+    "revision": "7825c947326df63ad912",
+    "url": "/Artem20201610/react_people-table-advanced/static/css/main.0ff47694.chunk.css"
+  },
+  {
+    "revision": "af95ec4e54be896ff0c4",
+    "url": "/Artem20201610/react_people-table-advanced/static/js/2.63b18781.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/Artem20201610/react_people-table-advanced/static/js/2.63b18781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7825c947326df63ad912",
+    "url": "/Artem20201610/react_people-table-advanced/static/js/main.406f1fdf.chunk.js"
+  },
+  {
+    "revision": "8c4d06e8222294a0e60b",
+    "url": "/Artem20201610/react_people-table-advanced/static/js/runtime-main.7f175076.js"
+  }
+]);
